6509172d6af0556a86192f47257c97a6a54dda0d
versedpro <ultraj0330@gmail.com>
2022-07-26 03:25:33 +0800

    feat: Update design & workflow

--- src/components/IFrameBox/index.tsx
+++ src/components/IFrameBox/index.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import './style.css';
 import { Icon } from '@iconify/react';
-// import { getMintInfo } from '../../api/mint';
-import LunaLogo from '../../assets/LunaLogo';
-// import { useWeb3React } from '@web3-react/core';
+import { SpinningCircles } from 'react-loading-icons';
 
 type ComponentProps = {
   active: boolean;
   nftImgUrl?: string;
-  collectionImgUrl?: string;
   collectionTitle: string;
   nftTitle: string;
   nftDescription: string;
   price: number;
+  maxSupply: number;
   mintsRemain: number | undefined;
   mintBtnDisabled: boolean;
+  bgColor: string | undefined;
   questions: string[];
   socialLinks: { [key: string]: boolean };
   onConnectWallet?: () => void;
@@ -27,18 +26,22 @@ type ComponentProps = {
   answers: string[];
   answersError?: boolean[];
   onAnswersChange: (index: number, value: string) => void;
+  mintProcessing: boolean;
+  mintSucceed: boolean;
+  setMintSucceed: (value: boolean) => void;
 };
 
 const IFrameBox: React.FC<ComponentProps> = ({
   active,
   nftImgUrl,
-  collectionImgUrl,
   collectionTitle,
   nftTitle,
   nftDescription,
   price,
+  maxSupply,
   mintsRemain,
   mintBtnDisabled,
+  bgColor,
   questions,
   socialLinks,
   onConnectWallet,
@@ -50,211 +53,191 @@ const IFrameBox: React.FC<ComponentProps> = ({
   onNftCountChange,
   answers,
   answersError = [false, false, false],
-  onAnswersChange
+  onAnswersChange,
+  mintProcessing = false,
+  mintSucceed = false,
+  setMintSucceed
 }): JSX.Element => {
-  // useEffect(() => {
-  //     getMintInfo('aaa', 'aaa')
-  //         .then(async (response: any) => {
-  //             console.log('getMintInfo response:', response);
-  //         })
-  //         .catch((error) => {
-  //             console.log('getMintInfo error:', error);
-  //         });
-  // }, []);
-  // const { active } = useWeb3React();
-
   return (
     <div className="container mx-auto">
       <div
-        style={{
-          background: '#1d1d1d'
-        }}
-        className={`flex flex-col md:flex-row text-left p-2 w-full md:min-h-[240px] md:max-h-[380px] box-border ${className}`}
+        className={`flex flex-col xl:flex-row text-left w-full sm:min-h-min xl:max-h-[400px] box-border bg-[${
+          bgColor ? bgColor : '#1d1d1d'
+        }] ${className}`}
       >
-        <div className="relative w-full min-h-[240px] md:w-[40%]">
-          <img src={nftImgUrl} width="100%" height="100%" alt="" className="object-fill min-h-[240px]" />
-          <div style={{ position: 'absolute', inset: 0 }}>
+        <div className="relative w-full min-h-[240px] sm:h-[400px] sm:w-[400px] items-center justify-center border-[1px] border-solid border-white sm:border-none">
+          <img src={nftImgUrl} width="100%" height="100%" alt="" className="object-cover" />
+          <div className="absolute" style={{ inset: 0 }}>
             <div
               style={{
-                width: '100%',
-                height: '60%',
-                background: 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(27, 28, 34, 0) 0.01%, #000000 100%)',
-                transform: 'rotate(-180deg)'
+                background: 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(27, 28, 34, 0) 0.01%, #000000 100%)'
               }}
+              className="w-full h-[60%] transform -rotate-180"
             ></div>
           </div>
-          <div
-            style={{ inset: 0 }}
-            className="flex absolute flex-col justify-between p-2 border-solid border-[2px] border-[white] md:border-none"
-          >
-            <div className="flex flex-col xl:flex-row justify-between items-center">
-              <div className="flex flex-row items-center">
-                <img src={collectionImgUrl} width={40} height={40} style={{ borderRadius: 100 }} alt="" />
-                <p style={{ fontSize: 20, color: 'white', marginLeft: 16 }}>{collectionTitle}</p>
-              </div>
-              <div className="flex flex-row items-center text-white">
-                {socialLinks['twitter'] && <Icon icon="mdi:twitter" fontSize={24} />}
-                {socialLinks['discord'] && <Icon icon="ic:baseline-discord" fontSize={24} style={{ marginLeft: 20 }} />}
-                {socialLinks['facebook'] && <Icon icon="gg:facebook" fontSize={24} style={{ marginLeft: 18 }} />}
-                {socialLinks['instagram'] && <Icon icon="mdi:instagram" fontSize={24} style={{ marginLeft: 18 }} />}
-              </div>
-            </div>
-            <div className="flex flex-row items-center p-2 mx-auto md:-ml-2 w-fit bg-[#000000BF]">
-              <p className="text-[14px] text-white m-0">Powered by</p>
-              {/* <img src={LunaLogo} width={16} height={16} alt="" style={{ marginLeft: 6 }} /> */}
-              <LunaLogo style={{ marginLeft: 6 }} />
-            </div>
-          </div>
         </div>
-        <div className="flex flex-col w-full md:w-[30%] p-2">
-          <p className="text-[20px] text-white">{nftTitle}</p>
-          <p className="text-[14px] text-[#9E9E9E] my-2 md:m-0">{nftDescription}</p>
-          <div className="flex flex-row text-white justify-between md:gap-[60px]">
-            <div>
-              <p>Price</p>
-              <p className="font-semibold">{active ? `${price} ETH` : '-'}</p>
+        <div className="flex flex-col sm:flex-row gap-4 pt-[38px] mx-4 xl:mx-16">
+          <div className="flex flex-col gap-16 w-full sm:w-[315px]">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-2">
+                <p className="font-normal text-[20px] leading-[24px] text-white">{nftTitle}</p>
+                <p className="font-normal text-[14px] leading-[18px] text-white">{collectionTitle}</p>
+              </div>
+              <p className="text-[14px] leading-[18px] h-[72px] tracking-[0.16px] flex items-center text-white">
+                {nftDescription}
+              </p>
+              <div className="flex flex-row text-white justify-between sm:gap-[80px]">
+                <div className="flex flex-col gap-1">
+                  <p className="font-normal text-[16px] leading-[22px] flex items-center">Price</p>
+                  <p className="font-semibold text-[16px] leading-[22px] flex items-center">
+                    {active ? `${price} ETH` : '-'}
+                  </p>
+                </div>
+                <div className="flex flex-col gap-1">
+                  <p className="font-normal text-[16px] leading-[22px] flex items-center">Total Mints</p>
+                  <p className="font-semibold text-[16px] leading-[22px] flex items-center">
+                    {!active ? '-' : maxSupply}
+                  </p>
+                </div>
+              </div>
             </div>
-            <div>
-              <p>Mints Remaining</p>
-              <p className="font-semibold">{!active ? '-' : mintsRemain ? mintsRemain : 'Unlimited'}</p>
-            </div>
+            {!active ? (
+              <button
+                onClick={onConnectWallet}
+                className="w-full p-[6px] text-[14px] font-normal border-[1px] border-solid border-white rounded-[4px] bg-none text-white cursor-pointer active:scale-[0.99]"
+              >
+                CONNECT WALLET
+              </button>
+            ) : (
+              <button
+                onClick={onDisconnectWallet}
+                className="w-full p-[6px] text-[14px] font-normal border-[1px] border-solid border-white rounded-[4px] bg-none text-white cursor-pointer"
+              >
+                DISCONNECT WALLET
+              </button>
+            )}
+          </div>
+          <div className="flex flex-row sm:flex-col justify-center sm:justify-start items-center gap-4 text-white">
+            {socialLinks['twitter'] && <Icon icon="mdi:twitter" fontSize={24} />}
+            {socialLinks['discord'] && <Icon icon="ic:baseline-discord" fontSize={24} />}
+            {socialLinks['facebook'] && <Icon icon="gg:facebook" fontSize={24} />}
+            {socialLinks['instagram'] && <Icon icon="mdi:instagram" fontSize={24} />}
           </div>
           <div
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              gap: 16,
-              marginTop: 16
-            }}
+            className={`flex flex-col w-full sm:w-[320px] sm:ml-16 mb-2 sm:mb-0 ${questions[0] ? 'gap-[30px]' : ''}`}
           >
-            <input
-              placeholder="Number of NFT"
-              value={nftCount}
-              onChange={(event) => onNftCountChange(event.target.value)}
-              style={{
-                width: '50%',
-                padding: '0 8px',
-                borderRadius: 4,
-                border: nftCountError ? '2px solid #EB5757' : 'none'
-              }}
-              className="number-of-nft__inut"
-            />
-            <button
-              disabled={mintBtnDisabled}
-              onClick={onMintNft}
-              style={{
-                width: '50%',
-                height: 34,
-                fontSize: 14,
-                fontWeight: 400,
-                border: ' 1px solid white',
-                borderRadius: 4,
-                background: 'none',
-                color: 'white',
-                cursor: 'pointer'
-              }}
-              className="mint-nft__btn"
-            >
-              MINT NFT
-            </button>
+            {active && (
+              <>
+                {mintProcessing ? (
+                  <div className="flex w-full h-full items-center justify-center">
+                    <SpinningCircles />
+                  </div>
+                ) : mintSucceed ? (
+                  <div className="flex flex-col gap-[34px] justify-center h-full">
+                    <p className="flex items-center align-center justify-center font-normal text-[20px] leading-[24px] text-white">
+                      {nftCount} NFT is(are) successfully minted.
+                    </p>
+                    <button
+                      className="h-[34px] text-[14px] font-normal border-[1px] border-solid border-white rounded-[4px] text-white"
+                      onClick={() => setMintSucceed(false)}
+                    >
+                      OK
+                    </button>
+                  </div>
+                ) : (
+                  <>
+                    <div className="flex flex-col gap-3">
+                      {questions[0] && (
+                        <div className="flex flex-col gap-2">
+                          <div className="flex flex-row justify-between gap-2">
+                            <p className="text-[12px] leading-[18px] font-normal text-white">
+                              {questions[0]} Question 1
+                            </p>
+                            {answersError[0] && (
+                              <p className="italic font-normal text-[12px] leading-[18px] text-white">required</p>
+                            )}
+                          </div>
+                          <input
+                            placeholder="Answer 1"
+                            value={answers[0]}
+                            onChange={(event) => onAnswersChange(0, event.target.value)}
+                            className={`w-full px-2 py-3 rounded-[4px] bg-[#252525] text-[12px] leading-[18px] text-white ${
+                              answersError[0] ? 'border-[2px] border-solid border-[#EB5757]' : 'border-none'
+                            }`}
+                          />
+                        </div>
+                      )}
+                      {questions[1] && (
+                        <div className="flex flex-col gap-2">
+                          <div className="flex flex-row justify-between gap-2">
+                            <p className="text-[12px] leading-[18px] font-normal text-white">
+                              {questions[1]} Question 2
+                            </p>
+                            {answersError[1] && (
+                              <p className="italic font-normal text-[12px] leading-[18px] text-white">required</p>
+                            )}
+                          </div>
+                          <input
+                            placeholder="Answer 2"
+                            value={answers[1]}
+                            onChange={(event) => onAnswersChange(1, event.target.value)}
+                            className={`w-full px-2 py-3 rounded-[4px] bg-[#252525] text-[12px] leading-[18px] text-white ${
+                              answersError[1] ? 'border-[2px] border-solid border-[#EB5757]' : 'border-none'
+                            }`}
+                          />
+                        </div>
+                      )}
+                      {questions[2] && (
+                        <div className="flex flex-col gap-2">
+                          <div className="flex flex-row justify-between gap-2">
+                            <p className="text-[12px] leading-[18px] font-normal text-white">
+                              {questions[2]} Question 3
+                            </p>
+                            {answersError[2] && (
+                              <p className="italic font-normal text-[12px] leading-[18px] text-white">required</p>
+                            )}
+                          </div>
+                          <textarea
+                            placeholder="Answer 3"
+                            value={answers[2]}
+                            onChange={(event) => onAnswersChange(2, event.target.value)}
+                            className={`w-full h-[66px] px-2 py-3 rounded-[4px] bg-[#252525] text-[12px] leading-[18px] text-white ${
+                              answersError[2] ? 'border-[2px] border-solid border-[#EB5757]' : 'border-none'
+                            }`}
+                          />
+                        </div>
+                      )}
+                    </div>
+                    <div className="flex flex-row gap-4 justify-between">
+                      <div className="w-[50%]">
+                        <input
+                          placeholder="Number of NFT"
+                          value={nftCount}
+                          onChange={(event) => onNftCountChange(event.target.value)}
+                          className={`w-full px-2 rounded-[4px] h-[34px] ${
+                            nftCountError ? 'border-[2px] border-solid border-[#EB5757]' : 'border-none'
+                          }`}
+                        />
+                        {nftCountError && (
+                          <p className="relative left-2 top-[5px] italic font-normal text-[12px] leading-[18px] text-white">
+                            required
+                          </p>
+                        )}
+                      </div>
+
+                      <button
+                        disabled={mintBtnDisabled && mintsRemain === 0}
+                        onClick={onMintNft}
+                        className="w-[50%] h-[34px] text-[14px] font-normal border-[1px] border-solid border-white rounded-[4px] bg-none text-white cursor-pointer active:enabled:scale-[0.99]"
+                      >
+                        MINT NFT
+                      </button>
+                    </div>
+                  </>
+                )}
+              </>
+            )}
           </div>
-          {!active ? (
-            <button
-              onClick={onConnectWallet}
-              style={{
-                width: '100%',
-                height: 34,
-                fontSize: 14,
-                fontWeight: 400,
-                border: ' 1px solid white',
-                borderRadius: 4,
-                background: 'none',
-                color: 'white',
-                cursor: 'pointer',
-                marginTop: 16
-              }}
-              className="connect-wallet__btn"
-            >
-              CONNECT WALLET
-            </button>
-          ) : (
-            <button
-              onClick={onDisconnectWallet}
-              style={{
-                width: '100%',
-                height: 34,
-                fontSize: 14,
-                fontWeight: 400,
-                border: ' 1px solid white',
-                borderRadius: 4,
-                background: 'none',
-                color: 'white',
-                cursor: 'pointer',
-                marginTop: 16
-              }}
-              className="connect-wallet__btn"
-            >
-              DISCONNECT WALLET
-            </button>
-          )}
-        </div>
-        <div
-          style={{
-            width: '30%',
-            display: 'flex',
-            flexDirection: 'column',
-            gap: 16,
-            marginLeft: 48
-          }}
-        >
-          {questions[0] && (
-            <div>
-              <p style={{ fontSize: 12, fontWeight: 400, color: '#9E9E9E' }}>{questions[0]}</p>
-              <input
-                placeholder="Answer 1"
-                value={answers[0]}
-                onChange={(event) => onAnswersChange(0, event.target.value)}
-                style={{
-                  width: '60%',
-                  padding: '12px 8px',
-                  borderRadius: 4,
-                  border: answersError[0] ? '2px solid #EB5757' : 'none'
-                }}
-              />
-            </div>
-          )}
-          {questions[1] && (
-            <div>
-              <p style={{ fontSize: 12, fontWeight: 400, color: '#9E9E9E' }}>{questions[1]}</p>
-              <input
-                placeholder="Answer 2"
-                value={answers[1]}
-                onChange={(event) => onAnswersChange(1, event.target.value)}
-                style={{
-                  width: '60%',
-                  padding: '12px 8px',
-                  borderRadius: 4,
-                  border: answersError[1] ? '2px solid #EB5757' : 'none'
-                }}
-              />
-            </div>
-          )}
-          {questions[2] && (
-            <div>
-              <p style={{ fontSize: 12, fontWeight: 400, color: '#9E9E9E' }}>{questions[2]}</p>
-              <input
-                placeholder="Answer 3"
-                value={answers[2]}
-                onChange={(event) => onAnswersChange(2, event.target.value)}
-                style={{
-                  width: '60%',
-                  padding: '12px 8px',
-                  borderRadius: 4,
-                  border: answersError[2] ? '2px solid #EB5757' : 'none'
-                }}
-              />
-            </div>
-          )}
         </div>
       </div>
     </div>
