51d374d3be0a705d442c7e1d0dc138c2cca88c6d
versedpro <ultraj0330@gmail.com>
2022-07-26 10:14:22 +0800

    chore: Add switch network

--- src/components/LunaCheckoutWidget/index.tsx
+++ src/components/LunaCheckoutWidget/index.tsx
@@ -129,7 +129,18 @@ const LunaCheckoutWidget: React.FC<ComponentProps> = ({ collectionId, username, 
     setAnswersError(updatedAnswersError);
   };
 
-  const handleConnectMetamask = () => {
+  const handleConnectMetamask = async () => {
+    const w: any = window;
+
+    await w.ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [
+        {
+          chainId: '0x4'
+        }
+      ]
+    });
+
     activate(testConnectors.injected);
   };
 
