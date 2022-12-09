6e9024622c0d0a20ce02690471befdc3f89c9e5e
versedpro <ultraj0330@gmail.com>
2022-12-09 09:16:19 +0800

    chore: Remove /api prefix from base api URL

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,5 +1,5 @@
-const BaseURL_Staging = 'https://creators-portals-api-staging-8lv8j.ondigitalocean.app/api';
-// const BaseURL_Product = 'https://creators-portals-api-staging-8lv8j.ondigitalocean.app/api';
+const BaseURL_Staging = 'https://creators-portals-api-staging-8lv8j.ondigitalocean.app';
+// const BaseURL_Product = 'https://creators-portals-api-staging-8lv8j.ondigitalocean.app';
 
 const SERVER_URL = BaseURL_Staging;
 
